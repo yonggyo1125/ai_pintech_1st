9e61396ba168c10de0fd10b44a6ff480141adfcd
yonggyo1125 <yonggyo00@kakao.com>
2024-10-07 13:21:15 +0900

    .

---
+++ 4. 자바 기초/source/day01/src/exam01/Ex01.java
@@ -0,0 +1,20 @@
+package exam01;
+
+public class Ex01 {
+    /**
+     * 설명 전용 주석
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*
+            여러줄 주석
+            여러줄 주석
+         */
+        int num1; // 변수 선언 - 공간 할당
+        num1 = 10; // 초기화 - 최초로 값을 저장하는 것, = 대입 연산자
+        //System.out.println(num1); 자바 코드 -> 주석으로 변경 -> 해석 X -> 실행 배제
+
+        int num2 = 20; // 선언과 동시에 초기화
+    }
+}
