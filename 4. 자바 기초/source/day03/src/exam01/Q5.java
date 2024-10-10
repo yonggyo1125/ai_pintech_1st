9e322b97300817b230e2a4749202bfaf0e4633d3
yonggyo1125 <yonggyo00@kakao.com>
2024-10-10 11:13:38 +0900

    수업 소스

--- 4. 자바 기초/source/day03/src/exam01/Q5.java
+++ 4. 자바 기초/source/day03/src/exam01/Q5.java
@@ -4,10 +4,20 @@ public class Q5 {
     public static void main(String[] args) {
         // 5. 구구단을 단보다 곱하는 수가 크거나 같은 경우만 출력하는 프로그램을 만들어 보세요.
         for (int i = 2; i <= 9; i++) { // 단
-
             System.out.printf("--- %d단 ---%n", i);
             for (int j = 1; j <= 9; j++) { // 곱하는 수
+                if (j < i) {
+                    continue;
+                }
+
                 System.out.printf("%d X %d = %d%n", i, j, i * j);
+
+                /*
+                if (j >= i) {
+                    System.out.printf("%d X %d = %d%n", i, j, i * j);
+                }
+
+                 */
             }
         }
     }
