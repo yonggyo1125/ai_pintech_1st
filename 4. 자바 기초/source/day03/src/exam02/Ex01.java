a86c0cf569277f6aa1a8b986f3492c448a16d166
yonggyo1125 <yonggyo00@kakao.com>
2024-10-10 11:26:04 +0900

    수업 소스

---
+++ 4. 자바 기초/source/day03/src/exam02/Ex01.java
@@ -0,0 +1,10 @@
+package exam02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        //int studentId1 = 20241401;
+        //int studentId2 = 20241402;
+        //int studentId99 = 20241499;
+        int[] studentIds = new int[99];
+    }
+}
