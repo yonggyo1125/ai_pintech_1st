807d4d08773c666ad286f233da4a1bf680551d67
yonggyo1125 <yonggyo00@kakao.com>
2024-10-10 13:21:34 +0900

    수업 소스

--- 4. 자바 기초/source/day03/src/exam02/Ex04.java
+++ 4. 자바 기초/source/day03/src/exam02/Ex04.java
@@ -6,11 +6,25 @@ public class Ex04 {
         //int[] nums = {10, 20, 30, 40};
         //int[] nums;
         //nums = {10, 20, 30, 40};
-        int[] nums;
-        nums = new int[] {10, 20, 30, 40};
+        //int[] nums;
+        //nums = new int[] {10, 20, 30, 40};
 
-        for (int i = 0; i <= 3; i++) {
+        int[] nums = {10, 20, 30, 40, 50, 60};
+
+        System.out.printf("length=%d%n", nums.length); // 배열 공간의 갯수
+
+        /*
+        for (int i = 0; i <= nums.length - 1; i++) {
             System.out.println(nums[i]);
+        }
+
+         */
+        for (int i = 0; i < nums.length; i++) { // 관례
+            System.out.println(nums[i]);
+        }
+
+        for (int num : nums) {
+            System.out.println(num);
         }
     }
 }
