4422f21946882391b93a270f9b6890394a4eff3e
yonggyo1125 <yonggyo00@kakao.com>
2024-10-14 12:03:25 +0900

    .

--- 4. 자바 기초/source/day05/src/exam02/Ex01.java
+++ 4. 자바 기초/source/day05/src/exam02/Ex01.java
@@ -3,10 +3,14 @@ package exam02;
 public class Ex01 {
     public static void main(String[] args) {
         Schedule s1 = new Schedule();
-        s1.year = 2024;
-        s1.month = 2;
-        s1.day = 31; // 31이라는 잘못된 값을 검증하고 대입을 통제하는 방법 X
+        //s1.year = 2024; // 통제 불가
+        //s1.month = 2;
+        //s1.day = 31; // 31이라는 잘못된 값을 검증하고 대입을 통제하는 방법 X
+        s1.setYear(2024);
+        s1.setMonth(2);
+        s1.setDay(31);
 
         s1.showDate();
+
     }
 }
--- 4. 자바 기초/source/day05/src/exam02/Schedule.java
+++ 4. 자바 기초/source/day05/src/exam02/Schedule.java
@@ -9,12 +9,28 @@ public class Schedule {
         year = _year;
     }
 
+    public int getYear() {
+        return year;
+    }
+
     public void setMonth(int _month) {
         month = _month;
     }
 
+    public int getMonth() {
+        return month;
+    }
+
     public void setDay(int _day) {
+        if (month == 2 && _day > 28) {  // 통제 가능
+            _day = 28;
+        }
+
         day = _day;
+    }
+
+    public int getDay() {
+        return day;
     }
 
     void showDate() {
