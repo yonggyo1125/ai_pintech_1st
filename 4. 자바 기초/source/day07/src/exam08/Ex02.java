da9e4848d2b86c6c2f914ffc3aea35c81526c38a
yonggyo1125 <yonggyo00@kakao.com>
2024-10-17 10:14:08 +0900

    .

--- 4. 자바 기초/source/day07/src/exam08/Ex02.java
+++ 4. 자바 기초/source/day07/src/exam08/Ex02.java
@@ -8,7 +8,7 @@ public class Ex02 {
         Calculator cal = new Calculator() {
             @Override
             public int add(int num1, int num2) {
-                return num1 - num2;
+                return num1 - 0num2;
             }
         };
 
