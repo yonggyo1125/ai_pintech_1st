bfdb36e071ca72a06f996582001646d6d85bace1
yonggyo1125 <yonggyo00@kakao.com>
2024-10-17 13:03:50 +0900

    .

--- 4. 자바 기초/source/day08/src/exam02/LoginService.java
+++ 4. 자바 기초/source/day08/src/exam02/LoginService.java
@@ -2,23 +2,16 @@ package exam02;
 
 public class LoginService {
 
-    public void process(String userId, String password) {
-        try {
+    public void process(String userId, String password) throws UserIdValidationException, PasswordValidationException {
+
             // userId - user01, password - 1234
             if (!userId.equals("user01")) { // user01과 불일치
-                throw new UserIdValidationException();
+                throw new UserIdValidationException("아이디가 일치하지 않습니다.");
             }
 
             if (!password.equals("1234")) { // 비번이 1234와 불일치
-                throw new PasswordValidationException();
+                throw new PasswordValidationException("비밀번호가 일치하지 않습니다.");
             }
-        } catch (UserIdValidationException e) {
-            /**
-             * String getMessage()
-             * void printStackTrace()
-             */
-        } catch (PasswordValidationException e) {
 
-        }
     }
 }
--- 4. 자바 기초/source/day08/src/exam02/PasswordValidationException.java
+++ 4. 자바 기초/source/day08/src/exam02/PasswordValidationException.java
@@ -1,4 +1,7 @@
 package exam02;
 
 public class PasswordValidationException extends Exception {
+    public PasswordValidationException(String message) {
+        super(message);
+    }
 }
