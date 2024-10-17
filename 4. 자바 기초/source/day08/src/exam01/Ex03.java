dacb82e344ffbcb6da072d82216ba1e5690608c4
yonggyo1125 <yonggyo00@kakao.com>
2024-10-17 10:46:46 +0900

    .

--- 4. 자바 기초/source/day08/src/exam01/Ex03.java
+++ 4. 자바 기초/source/day08/src/exam01/Ex03.java
@@ -2,16 +2,28 @@ package exam01;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Ex03 {
     public static void main(String[] args) {
+        FileInputStream fis = null;
         try {
-            FileInputStream fis = new FileInputStream("a.txt");
+            fis = new FileInputStream("b.txt");
 
             fis.close();
             System.out.println("자원 해제!");
-        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e2) {
+                    e2.printStackTrace();
+                }
+
+            }
+            System.out.println("자원 해제!");
         }
     }
 }
