84425a5c133a006d901640e33e2983eeca610a2f
yonggyo1125 <yonggyo00@kakao.com>
2024-10-18 11:59:49 +0900

    .

---
+++ 4. 자바 기초/source/day09/src/exam01/Ex04.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class cls = Class.forName("exam01.Person");
+        System.out.println(cls);
+    }
+}
