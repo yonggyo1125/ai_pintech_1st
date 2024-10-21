4fde9113853d21ad805858804c8891cdf9e725e5
yonggyo1125 <yonggyo00@kakao.com>
2024-10-21 10:44:01 +0900

    .

--- 4. 자바 기초/source/day10/src/exam01/Transportation.java
+++ 4. 자바 기초/source/day10/src/exam01/Transportation.java
@@ -1,7 +1,17 @@
 package exam01;
 
 public enum Transportation {
-    BUS,
-    SUBWAY,
-    TAXI
+    BUS("버스"),
+    SUBWAY("지하철"),
+    TAXI("택시");
+
+    private final String title;
+
+    Transportation(String title) { // private
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
