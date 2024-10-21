3d99f35010e42b144100026774882d8a24b5584c
yonggyo1125 <yonggyo00@kakao.com>
2024-10-21 11:22:41 +0900

    .

--- 4. 자바 기초/source/day10/src/exam01/Transportation.java
+++ 4. 자바 기초/source/day10/src/exam01/Transportation.java
@@ -1,9 +1,24 @@
 package exam01;
 
 public enum Transportation {
-    BUS("버스"),
-    SUBWAY("지하철"),
-    TAXI("택시");
+    BUS("버스") {
+        @Override
+        public int getTotal(int person) {
+            return 1500 * person;
+        }
+    },
+    SUBWAY("지하철") {
+        @Override
+        public int getTotal(int person) {
+            return 1400 * person;
+        }
+    },
+    TAXI("택시") {
+        @Override
+        public int getTotal(int person) {
+            return 4500 * person;
+        }
+    };
 
     private final String title;
 
@@ -14,4 +29,6 @@ public enum Transportation {
     public String getTitle() {
         return title;
     }
+
+    public abstract int getTotal(int person);
 }
