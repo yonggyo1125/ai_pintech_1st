769b5b2fbfa907ed4341a355b5c6a15818d17a7f
yonggyo1125 <yonggyo00@kakao.com>
2024-10-21 13:20:32 +0900

    .

--- 4. 자바 기초/source/day10/src/exam02/MyAnno.java
+++ 4. 자바 기초/source/day10/src/exam02/MyAnno.java
@@ -8,5 +8,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE, ElementType.METHOD}) // 적용 대상, 클래스명 위, 메서드드 위
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnno {
-    String value(); // 기본 설정 항목
+    String[] value(); // 기본 설정 항목
+    int min() default 1;
+    int max() default 10;
 }
