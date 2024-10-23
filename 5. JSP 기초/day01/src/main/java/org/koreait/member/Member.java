a5722a876e9b2c96da23e54ae1fbc11d420dee7f
yonggyo1125 <yonggyo00@kakao.com>
2024-10-23 13:21:20 +0900

    .

--- 5. JSP 기초/day01/src/main/java/org/koreait/member/Member.java
+++ 5. JSP 기초/day01/src/main/java/org/koreait/member/Member.java
@@ -1,13 +1,20 @@
 package org.koreait.member;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Setter @Getter @ToString
+//@Setter @Getter @ToString
+//@Data
+//@NoArgsConstructor(access=AccessLevel.PRIVATE)
+//@AllArgsConstructor
+//@RequiredArgsConstructor
+@Builder
+@ToString
 public class Member {
     private long seq; // 회원 번호
     private String email; // 이메일
+
+    //@NonNull
+    //@ToString.Exclude // ToString에서 배제
     private String password;
 
 }
--- 5. JSP 기초/day01/src/test/java/exam01/Ex01.java
+++ 5. JSP 기초/day01/src/test/java/exam01/Ex01.java
@@ -6,11 +6,25 @@ import org.koreait.member.Member;
 public class Ex01 {
     @Test
     void test1() {
-        Member member = new Member();
+        //Member member = new Member(1L, "user01@test.org", "12345678");
+        //Member member = new Member("user01@test.org", "12345678");
+
+        /*
         member.setSeq(1L);
         member.setEmail("user01@test.org");
         member.setPassword("12345678");
+        */
+        //System.out.println(member);
+    }
 
+    @Test
+    void test2() {
+        //Member member = new Member();
+        Member member = Member.builder()
+                .seq(1L)
+                .email("user01@test.org")
+                .password("12345678")
+                .build();
         System.out.println(member);
     }
 }
