05f5e094cc723ecc1ceca1dea6f8b5c006337754
yonggyo1125 <yonggyo00@kakao.com>
2024-10-31 16:24:49 +0900

    .

---
+++ 4. 자바 기초/source/day15/src/exam01/Ex01.java
@@ -0,0 +1,20 @@
+package exam01;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        List<String> items = new LinkedList<>();
+        items.add("항목1");
+        items.add("항목2");
+        items.add("항목3");
+        items.add("항목4");
+        items.add("항목5");
+
+        for (String item : items) {
+            System.out.println(item);
+        }
+    }
+}
