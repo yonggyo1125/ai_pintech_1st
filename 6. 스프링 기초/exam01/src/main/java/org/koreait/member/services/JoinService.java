d6752a4394f3ea28d4c750d401b02ff9c2a9cc73
yonggyo1125 <yonggyo00@kakao.com>
2024-11-01 15:24:45 +0900

    .

--- 6. 스프링 기초/exam01/src/main/java/org/koreait/member/services/JoinService.java
+++ 6. 스프링 기초/exam01/src/main/java/org/koreait/member/services/JoinService.java
@@ -9,36 +9,30 @@ import org.koreait.member.validators.JoinValidator;
  * 회원가입 기능
  */
 public class JoinService {
-    // 구성
-    private Validator<RequestJoin> joinValidator;
 
+    private Validator<RequestJoin> validator;
 
     /**
      * 의존 관계
+     * JoinService 객체를 생성하는데 validator가 필수!
      *
-     * @param joinValidator
-     * - JoinService 객체 생성을 위해서는 joinValidator가 필수!
+     * @param validator
      */
-    public JoinService(Validator<RequestJoin> joinValidator) {
-        this.joinValidator = joinValidator;
+    public JoinService(Validator<RequestJoin> validator) {
+        this.validator = validator;
     }
 
     /**
      * 연관 관계
-     * JoinService 객체를 생성할떄 필수는 아니기때문
-     * @param joinValidator
+     * JoinSevice 객체를 생성하는데 필수는 아니다, 연관성이 있다.
+     * @param validator
      */
     /*
-    public void setValidator(Validator<RequestJoin> joinValidator) {
-        this.joinValidator = joinValidator;
+    public void setValidator(Validator<RequestJoin> validator) {
+        this.validator = validator;
     }
     */
-    /**
-     * 가입 처리
-     *
-     * RequestJoin - DTO(Data Transfer Object) : 사용자가 입력한 값을 전달
-     */
     public void process(RequestJoin form) {
-
+        validator.check(form);
     }
 }
--- 6. 스프링 기초/exam01/src/main/java/org/koreait/member/services/ServiceContainer.java
+++ 6. 스프링 기초/exam01/src/main/java/org/koreait/member/services/ServiceContainer.java
@@ -1,12 +1,18 @@
 package org.koreait.member.services;
 
+import org.koreait.global.validators.Validator;
+import org.koreait.member.controllers.RequestJoin;
+import org.koreait.member.validators.AdvancedJoinValidator;
 import org.koreait.member.validators.JoinValidator;
 
 public class ServiceContainer {
 
-    public JoinValidator joinValidator() {
-        return new JoinValidator();
+    public Validator<RequestJoin> joinValidator() {
+        //return new JoinValidator();
+        return new AdvancedJoinValidator();
     }
+
+
 
     public JoinService joinService() {
         return new JoinService(joinValidator());
