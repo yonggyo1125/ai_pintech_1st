19840e36f52631fa9e515b7d790935edab63cf1a
yonggyo1125 <yonggyo00@kakao.com>
2024-11-04 10:42:46 +0900

    .

--- 6. 스프링 기초/exam02/src/main/java/org/koreait/member/services/JoinService.java
+++ 6. 스프링 기초/exam02/src/main/java/org/koreait/member/services/JoinService.java
@@ -1,5 +1,7 @@
 package org.koreait.member.services;
 
+import org.koreait.member.controllers.RequestJoin;
+
 /**
  * 회원 가입 기능
  */
@@ -8,7 +10,7 @@ public class JoinService {
     /**
      * 가입 처리
      */
-    public void process() {
+    public void process(RequestJoin form) {
 
     }
 }
---
+++ 6. 스프링 기초/exam02/src/main/java/org/koreait/member/validators/JoinValidator.java
@@ -0,0 +1,12 @@
+package org.koreait.member.validators;
+
+import org.koreait.member.controllers.RequestJoin;
+
+/**
+ * 회원 가입 요청 데이터 검증
+ */
+public class JoinValidator {
+    public void validate(RequestJoin form) {
+
+    }
+}
