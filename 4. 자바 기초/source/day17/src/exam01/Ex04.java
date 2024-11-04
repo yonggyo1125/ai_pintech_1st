a5fcff63843337f27e632abfa963d778b732c37a
yonggyo1125 <yonggyo00@kakao.com>
2024-11-04 16:41:16 +0900

    .

--- 4. 자바 기초/source/day17/src/exam01/Ex04.java
+++ 4. 자바 기초/source/day17/src/exam01/Ex04.java
@@ -8,10 +8,14 @@ public class Ex04 {
         int[] nums2 = {1, 2, 3, 4, 5, 6};
         boolean isSame = Arrays.equals(nums1, nums2);
         System.out.println(isSame);
+        System.out.println("nums1:" + nums1);
+        System.out.println("nums1:" + Arrays.toString(nums1));
 
         int[][] nums3 = {{1,2,3}, {4,5,6}};
         int[][] nums4 = {{1,2,3}, {4,5,6}};
         boolean isSame2 = Arrays.deepEquals(nums3, nums4);
         System.out.println(isSame2);
+        System.out.println("nums3:" + Arrays.toString(nums3));
+        System.out.println("nums3:" + Arrays.deepToString(nums3));
     }
 }
