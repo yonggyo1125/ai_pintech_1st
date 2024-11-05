8144af4d9c766e35936cc1898f1ea6c1af083af3
yonggyo1125 <yonggyo00@kakao.com>
2024-11-05 10:04:44 +0900

    ..

--- 6. 스프링 기초/exam02/src/test/java/org/koreait/exam02/Ex01.java
+++ 6. 스프링 기초/exam02/src/test/java/org/koreait/exam02/Ex01.java
@@ -2,6 +2,8 @@ package org.koreait.exam02;
 
 import org.junit.jupiter.api.Test;
 import org.koreait.global.configs.AppCtx4;
+import org.koreait.member.controllers.RequestJoin;
+import org.koreait.member.services.InfoService;
 import org.koreait.member.services.JoinService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,6 +13,17 @@ public class Ex01 {
     void test1() {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx4.class);
         JoinService joinService = ctx.getBean(JoinService.class);
+        InfoService infoService = ctx.getBean(InfoService.class);
+
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user01@test.org");
+        form.setPassword("12345678");
+        form.setConfirmPassword("12345678");
+        form.setUserName("사용자01");
+        form.setAgree(true);
+
+        joinService.process(form);
+        infoService.print();
 
         ctx.close();
     }
