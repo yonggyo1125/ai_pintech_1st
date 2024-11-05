bb37d47282c5f1026e44ef7f35331a1b98454b7e
yonggyo1125 <yonggyo00@kakao.com>
2024-11-05 15:49:10 +0900

    .

---
+++ 4. 자바 기초/source/day18/src/exam01/Ex01.java
@@ -0,0 +1,16 @@
+package exam01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        String[] items1 = {"항목1", "항목1", "항목1", "항목2", "항목3", "항목4", "항목5"};
+        List<String> cItems1 = Arrays.stream(items1).distinct().map(s -> String.format("**%s**", s)).toList();
+        System.out.println(cItems1);
+
+        List<String> items2 = List.of("항목1", "항목1", "항목1", "항목2", "항목3", "항목4", "항목5");
+        List<String> cItems2 = items2.stream().distinct().map(s -> String.format("**%s**", s)).toList();
+        System.out.println(cItems2);
+    }
+}
