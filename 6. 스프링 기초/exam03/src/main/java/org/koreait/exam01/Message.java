210689766afb7d2b938e5c5ca56bbb0a9254707e
yonggyo1125 <yonggyo00@kakao.com>
2024-11-05 12:39:42 +0900

    .

--- 6. 스프링 기초/exam03/src/main/java/org/koreait/exam01/Message.java
+++ 6. 스프링 기초/exam03/src/main/java/org/koreait/exam01/Message.java
@@ -1,6 +1,13 @@
 package org.koreait.exam01;
 
-public class Message {
+import org.springframework.beans.factory.InitializingBean;
+
+public class Message implements InitializingBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet() 호출!");
+    }
+
     public void send(String message) {
         System.out.printf("메세지:%s, 전송 완료!%n", message);
     }
