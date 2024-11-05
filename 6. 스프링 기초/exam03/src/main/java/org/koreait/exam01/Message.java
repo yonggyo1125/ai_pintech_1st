53f47143058476c3be3d1d67c7d8d9a25184fed2
yonggyo1125 <yonggyo00@kakao.com>
2024-11-05 12:24:11 +0900

    .

---
+++ 6. 스프링 기초/exam03/src/main/java/org/koreait/exam01/Message.java
@@ -0,0 +1,7 @@
+package org.koreait.exam01;
+
+public class Message {
+    public void send(String message) {
+        System.out.printf("메세지:%s, 전송 완료!%n", message);
+    }
+}
---
+++ 6. 스프링 기초/exam03/src/main/java/org/koreait/exam01/config/AppCtx.java
@@ -0,0 +1,7 @@
+package org.koreait.exam01.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+}
