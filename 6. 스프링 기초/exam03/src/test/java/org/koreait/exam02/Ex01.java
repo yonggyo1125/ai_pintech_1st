f34fd653e1c6c92b03f997e665f91d3e43e354a8
yonggyo1125 <yonggyo00@kakao.com>
2024-11-05 15:23:36 +0900

    .

--- 6. 스프링 기초/exam03/src/test/java/org/koreait/exam02/Ex01.java
+++ 6. 스프링 기초/exam03/src/test/java/org/koreait/exam02/Ex01.java
@@ -26,12 +26,26 @@ public class Ex01 {
 
     @Test
     void test2() {
-        ProxyCalculator cal1 = new ProxyCalculator(new ImplCalculator());
+        Calculator cal1 = new ProxyCalculator(new ImplCalculator());
         long result1 = cal1.factorial(10L);
         System.out.printf("cal1=%d%n", result1);
 
-        ProxyCalculator cal2 = new ProxyCalculator(new RecCalculator());
+        Calculator cal2 = new ProxyCalculator(new RecCalculator());
         long result2 = cal2.factorial(10L);
         System.out.printf("cal2=%d%n", result2);
+    }
+
+    @Test
+    void test3() {
+        Calculator cal = new CachedCalculator(new ProxyCalculator(new RecCalculator()));
+
+        long r1 = cal.factorial(10L);
+        System.out.printf("r1=%d%n", r1);
+
+        long r2 = cal.factorial(10L);
+        System.out.printf("r2=%d%n", r2);
+
+        long r3 = cal.factorial(10L);
+        System.out.printf("r3=%d%n", r3);
     }
 }
