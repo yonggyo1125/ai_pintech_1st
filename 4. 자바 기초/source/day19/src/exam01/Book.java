95059307e7a6057df739944671889d81e49c71a2
yonggyo1125 <yonggyo00@kakao.com>
2024-11-06 16:52:12 +0900

    .

--- 4. 자바 기초/source/day19/src/exam01/Book.java
+++ 4. 자바 기초/source/day19/src/exam01/Book.java
@@ -19,4 +19,12 @@ public class Book {
     public void setAuthor(String author) {
         this.author = author;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
