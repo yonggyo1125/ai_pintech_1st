011958b50970138ecf94cb16873e9b61985b68be
yonggyo1125 <yonggyo00@kakao.com>
2024-11-07 13:20:52 +0900

    .

---
+++ 6. 스프링 기초/exam04/src/main/java/org/koreait/member/entities/Member.java
@@ -0,0 +1,15 @@
+package org.koreait.member.entities;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Member {
+    private long seq;
+    private String email;
+    private String password;
+    private String username;
+    private LocalDateTime regdt;
+    private LocalDateTime modDt;
+}
--- 6. 스프링 기초/exam04/src/test/java/org/koreait/exam02/Ex01.java
+++ 6. 스프링 기초/exam04/src/test/java/org/koreait/exam02/Ex01.java
@@ -3,10 +3,16 @@ package org.koreait.exam02;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.koreait.global.configs.AppCtx;
+import org.koreait.member.entities.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @SpringJUnitConfig
 @ContextConfiguration(classes = AppCtx.class)
@@ -19,8 +25,29 @@ public class Ex01 {
     @DisplayName("INSERT 쿼리 테스트")
     void test1() {
         String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USERNAME) VALUES (MEMBER_SEQ.nextval, ?, ?, ?)";
-        int affectedRows = jdbcTemplate.update(sql, "user03@test.org", "123456", "사용자03");
+        int affectedRows = jdbcTemplate.update(sql, "user05@test.org", "123456", "사용자03");
         System.out.println(affectedRows);
+
+    }
+    
+    @Test
+    @DisplayName("UPDATE 쿼리 테스트")
+    void test2() {
+        String sql = "UPDATE MEMBER SET USERNAME=? WHERE EMAIL LIKE 'user%'";
+        int affectedRows = jdbcTemplate.update(sql, "(수정)사용자명");
+        System.out.println(affectedRows);
+    }
+
+    @Test
+    @DisplayName("SELECT 쿼리 테스트 - 전체 목록")
+    void test3() {
+        String sql = "SELECT * FROM MEMBER";
+        List<Member> members = jdbcTemplate.query(sql, new RowMapper<Member>() {
+            @Override
+            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return null;
+            }
+        });
 
     }
 }
