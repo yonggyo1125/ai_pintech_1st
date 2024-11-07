533c52c634b86e57b1563215928885f939f6523f
yonggyo1125 <yonggyo00@kakao.com>
2024-11-07 16:01:41 +0900

    .

--- 6. 스프링 기초/exam04/src/main/java/org/koreait/member/entities/Member.java
+++ 6. 스프링 기초/exam04/src/main/java/org/koreait/member/entities/Member.java
@@ -1,12 +1,14 @@
 package org.koreait.member.entities;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Member {
-    private long seq;
+    @Id // 기본키로 사용할 컬럼
+    private Long seq;
     private String email;
     private String password;
     private String username;
