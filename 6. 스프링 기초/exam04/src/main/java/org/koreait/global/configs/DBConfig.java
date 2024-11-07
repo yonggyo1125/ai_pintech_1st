c289bc24d7a4ec22eae7d538119b595fd3c128bf
yonggyo1125 <yonggyo00@kakao.com>
2024-11-07 15:22:37 +0900

    .

--- 6. 스프링 기초/exam04/src/main/java/org/koreait/global/configs/DBConfig.java
+++ 6. 스프링 기초/exam04/src/main/java/org/koreait/global/configs/DBConfig.java
@@ -3,14 +3,19 @@ package org.koreait.global.configs;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-public class DBConfig {
+@EnableJdbcRepositories("org.koreait")
+public class DBConfig extends AbstractJdbcConfiguration {
 
     @Bean(destroyMethod = "close")
     public DataSource dataSource() {
@@ -38,5 +43,10 @@ public class DBConfig {
         tm.setDataSource(dataSource());
 
         return tm;
+    }
+
+    @Bean
+    public NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 }
--- 6. 스프링 기초/exam04/src/test/java/org/koreait/exam02/Ex01.java
+++ 6. 스프링 기초/exam04/src/test/java/org/koreait/exam02/Ex01.java
@@ -5,16 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.koreait.global.configs.AppCtx;
 import org.koreait.member.entities.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
+@Transactional
 @SpringJUnitConfig
 @ContextConfiguration(classes = AppCtx.class)
 public class Ex01 {
@@ -26,7 +29,7 @@ public class Ex01 {
     @DisplayName("INSERT 쿼리 테스트")
     void test1() {
         String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USERNAME) VALUES (MEMBER_SEQ.nextval, ?, ?, ?)";
-        int affectedRows = jdbcTemplate.update(sql, "user05@test.org", "123456", "사용자03");
+        int affectedRows = jdbcTemplate.update(sql, "user99@test.org", "123456", "사용자99");
         System.out.println(affectedRows);
 
     }
@@ -42,25 +45,49 @@ public class Ex01 {
     @Test
     @DisplayName("SELECT 쿼리 테스트 - 전체 목록")
     void test3() {
-        String sql = "SELECT * FROM MEMBER";
-        List<Member> members = jdbcTemplate.query(sql, (rs, i) -> {
-
-                Member member = new Member();
-                member.setSeq(rs.getLong("SEQ"));
-                member.setEmail(rs.getString("EMAIL"));
-                member.setPassword(rs.getString("PASSWORD"));
-                member.setUsername(rs.getString("USERNAME"));
-                member.setRegdt(rs.getTimestamp("regdt").toLocalDateTime());
-
-                Timestamp modDt = rs.getTimestamp("moddt");
-                if (modDt != null) {
-                    member.setModdt(modDt.toLocalDateTime());
-                }
-
-                return member;
-            }
-        });
+        String sql = "SELECT * FROM MEMBER WHERE USERNAME LIKE ?";
+        List<Member> members = jdbcTemplate.query(sql, this::mapper, "%수정%");
 
         members.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("SELECT 쿼리 - 단일 조회")
+    void test4() {
+        try {
+            String sql = "SELECT * FROM MEMBER WHERE EMAIL LIKE ?";
+            Member item = jdbcTemplate.queryForObject(sql, this::mapper, "%user%");
+            System.out.println(item);
+        } catch (DataAccessException e) {
+            System.out.println("데이터 없음");
+        }
+    }
+
+    @Test
+    @DisplayName("단일 데이터 조회 - 사용명")
+    void test5() {
+        String sql = "SELECT USERNAME FROM MEMBER WHERE EMAIL='user03@test.org'";
+        String username = jdbcTemplate.queryForObject(sql, String.class);
+        System.out.println(username);
+
+        String sql2 = "SELECT COUNT(*) FROM MEMBER";
+        long total = jdbcTemplate.queryForObject(sql2, long.class);
+        System.out.println(total);
+    }
+
+    private Member mapper(ResultSet rs, int i) throws SQLException {
+        Member member = new Member();
+        member.setSeq(rs.getLong("SEQ"));
+        member.setEmail(rs.getString("EMAIL"));
+        member.setPassword(rs.getString("PASSWORD"));
+        member.setUsername(rs.getString("USERNAME"));
+        member.setRegdt(rs.getTimestamp("regdt").toLocalDateTime());
+
+        Timestamp modDt = rs.getTimestamp("moddt");
+        if (modDt != null) {
+            member.setModdt(modDt.toLocalDateTime());
+        }
+
+        return member;
     }
 }
