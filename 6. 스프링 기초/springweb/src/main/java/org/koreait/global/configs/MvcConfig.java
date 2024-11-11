6914f4e2a0fee4e7ae22df40b44011fed7495953
yonggyo1125 <yonggyo00@kakao.com>
2024-11-11 10:21:32 +0900

    .

--- 6. 스프링 기초/springweb/src/main/java/org/koreait/global/configs/MvcConfig.java
+++ 6. 스프링 기초/springweb/src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -1,7 +1,22 @@
 package org.koreait.global.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/templates/", ".jsp");
+    }
 }
