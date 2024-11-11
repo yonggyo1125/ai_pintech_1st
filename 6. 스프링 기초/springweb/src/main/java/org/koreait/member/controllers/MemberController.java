886307dbe82f88a87255e0b18b68737aba1f00cf
yonggyo1125 <yonggyo00@kakao.com>
2024-11-11 12:23:18 +0900

    .

--- 6. 스프링 기초/springweb/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 6. 스프링 기초/springweb/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,21 +1,62 @@
 package org.koreait.member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+    @Autowired
+    private HttpSession session;
+    /*
     @GetMapping("/member/login")
-    public String login(/*@RequestParam("name") String name */) {
+    public String login(/*@RequestParam("name") String name ) {
 
        // System.out.printf("name:%s%n", name);
 
+     /*   return "member/form";
+     } */
+
+    @GetMapping("/member/login")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("member/form");
+
+        return mv;
+    }
+   /*
+    public String login() {
+
+        System.out.println(request);
+        System.out.println(response);
+        System.out.println(session);
+
+        return "member/form"; // 뷰를 찾기 위한 정보
+        // /WEB-INF/templates/member/form.jsp
+    } */
+    /*
+    public String login(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        System.out.println(request);
+        System.out.println(response);
+        System.out.println(session);
+
         return "member/form";
     }
-
+    */
     @PostMapping("/member/login")
     public String loginPs(RequestLogin form) {
 
