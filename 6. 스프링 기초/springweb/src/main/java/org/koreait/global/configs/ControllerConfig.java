faf6c36981fbba5ef8901bbe0ab582fad0836ec1
yonggyo1125 <yonggyo00@kakao.com>
2024-11-11 10:41:54 +0900

    .

--- 6. 스프링 기초/springweb/src/main/java/org/koreait/global/configs/ControllerConfig.java
+++ 6. 스프링 기초/springweb/src/main/java/org/koreait/global/configs/ControllerConfig.java
@@ -1,7 +1,13 @@
 package org.koreait.global.configs;
 
+import org.koreait.member.controllers.MemberController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ControllerConfig {
+    @Bean
+    public MemberController memberController() {
+        return new MemberController();
+    }
 }
