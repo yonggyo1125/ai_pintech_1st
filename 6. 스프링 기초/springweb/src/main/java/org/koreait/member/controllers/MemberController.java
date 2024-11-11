5a63375ded96ed3ce9eb81c0d827f87d9100a6bb
yonggyo1125 <yonggyo00@kakao.com>
2024-11-11 10:53:18 +0900

    .

--- 6. 스프링 기초/springweb/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 6. 스프링 기초/springweb/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -2,12 +2,16 @@ package org.koreait.member.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
 
     @GetMapping("/member/login")
-    public String login() {
+    public String login(@RequestParam("name") String name) {
+
+        System.out.printf("name:%s%n", name);
+
         return "member/form";
     }
 }
--- 6. 스프링 기초/springweb/src/main/webapp/WEB-INF/templates/member/form.jsp
+++ 6. 스프링 기초/springweb/src/main/webapp/WEB-INF/templates/member/form.jsp
@@ -1,2 +1,17 @@
 <%@ page contentType='text/html; charset=UTF-8' %>
 <h1>로그인 페이지</h1>
+<form method="POST" action="join">
+    <dl>
+        <dt>이메일</dt>
+        <dd>
+            <input type="text" name="email">
+        </dd>
+    </dl>
+    <dl>
+        <dt>비밀번호</dt>
+        <dd>
+            <input type="password" name="password">
+        </dd>
+    </dl>
+    <button type="submit">로그인</button>
+</form>
