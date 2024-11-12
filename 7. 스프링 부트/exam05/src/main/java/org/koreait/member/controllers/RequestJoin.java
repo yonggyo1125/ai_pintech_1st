4f3a8eb7572e87102b3553d4f03849d232064443
yonggyo1125 <yonggyo00@kakao.com>
2024-11-12 12:12:29 +0900

    .

---
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -0,0 +1,11 @@
+package org.koreait.member.controllers;
+
+import lombok.Data;
+
+@Data
+public class RequestJoin {
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String name;
+}
---
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/entities/Member.java
@@ -0,0 +1,7 @@
+package org.koreait.member.entities;
+
+import lombok.Data;
+
+@Data
+public class Member {
+}
