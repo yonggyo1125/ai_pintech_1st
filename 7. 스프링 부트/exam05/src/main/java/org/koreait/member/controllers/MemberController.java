b5a937f6584cd42499bec17929f67885d708e72c
yonggyo1125 <yonggyo00@kakao.com>
2024-11-12 12:22:14 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,6 +1,7 @@
 package org.koreait.member.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     @GetMapping("/join")
-    public String join() {
+    public String join(Model model) {
+
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user01@test.org");
+        form.setPassword("12345678");
+        form.setConfirmPassword("12345678");
+        form.setName("사용자01");
+
+        model.addAttribute("requestJoin", form);
 
         return "member/join";
     }
