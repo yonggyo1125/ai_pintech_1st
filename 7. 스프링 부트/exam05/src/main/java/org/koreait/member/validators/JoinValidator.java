52da5dff8e9dc21da8f28bea1144dce09071a899
yonggyo1125 <yonggyo00@kakao.com>
2024-11-14 10:48:57 +0900

    .

---
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/validators/JoinValidator.java
@@ -0,0 +1,30 @@
+package org.koreait.member.validators;
+
+import org.koreait.member.controllers.RequestJoin;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class JoinValidator implements Validator {
+    /**
+     * 검증을 할 커맨드 객체를 한정
+     *  RequestJoin 커맨드 객체만을 검증할거야!
+     */
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(RequestJoin.class);
+    }
+
+    /**
+     * 커맨드 객체 검증 수행
+     *
+     * Object target : 검증할 커맨드 객체
+     * Errors errors
+     *  - 검증 실패시 전달할 메세지를 설정
+     */
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        RequestJoin form = (RequestJoin)target;
+
+    }
+}
