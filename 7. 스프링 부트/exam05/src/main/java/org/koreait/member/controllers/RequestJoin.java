da34f93a1bd70ae17a88c111bb2aacd31cb54ee9
yonggyo1125 <yonggyo00@kakao.com>
2024-11-14 09:54:12 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/member/controllers/RequestJoin.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -8,4 +8,6 @@ public class RequestJoin {
     private String password;
     private String confirmPassword;
     private String name;
+    private String mobile; // 휴대전화 번호
+    private boolean agree; // 회원가입 약관 동의 여부
 }
