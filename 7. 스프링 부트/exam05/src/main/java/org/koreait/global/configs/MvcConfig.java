45dfee20c7f9b025b3ab803656d74353a0728894
yonggyo1125 <yonggyo00@kakao.com>
2024-11-14 15:38:53 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/MvcConfig.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -1,9 +1,22 @@
 package org.koreait.global.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.member.validators.JoinValidator;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+    private final JoinValidator joinValidator;
 
+    /**
+     * 모든 @Valid가 적용된 커맨드 객체 검증에 사용
+     * @return
+     */
+    @Override
+    public Validator getValidator() {
+        return joinValidator;
+    }
 }
