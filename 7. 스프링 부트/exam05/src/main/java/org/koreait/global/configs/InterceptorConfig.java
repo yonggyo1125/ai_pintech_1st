9b299ed01217c2b0b40ac9ff1818c1f7646937b1
yonggyo1125 <yonggyo00@kakao.com>
2024-11-18 15:58:42 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/InterceptorConfig.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/InterceptorConfig.java
@@ -1,8 +1,19 @@
 package org.koreait.global.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.interceptors.CommonInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
+    private final CommonInterceptor commonInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(commonInterceptor); // /** 동일
+                //.addPathPatterns("/**");
+    }
 }
