ee7840cb25459884667048beaad287334dc1c5b6
yonggyo1125 <yonggyo00@kakao.com>
2024-11-18 16:16:15 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/InterceptorConfig.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/InterceptorConfig.java
@@ -2,6 +2,7 @@ package org.koreait.global.configs;
 
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.interceptors.CommonInterceptor;
+import org.koreait.global.interceptors.MemberOnlyInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,11 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorConfig implements WebMvcConfigurer {
 
     private final CommonInterceptor commonInterceptor;
-
+    private final MemberOnlyInterceptor memberOnlyInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(commonInterceptor); // /** 동일
                 //.addPathPatterns("/**");
+
+        registry.addInterceptor(memberOnlyInterceptor)
+                .addPathPatterns("/mypage/**"); // /mypage를 포함한 모든 하위 경로
     }
 }
