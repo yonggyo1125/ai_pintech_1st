3759491b9ad94b2f76d70cf92d0ccba8af91ff2d
yonggyo1125 <yonggyo00@kakao.com>
2024-11-18 15:16:44 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/question/QuestionController.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/question/QuestionController.java
@@ -1,5 +1,6 @@
 package org.koreait.question;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -32,5 +33,31 @@ public class QuestionController {
         log.info("step:{} / {}", 2, form.toString());
 
         return "question/step3";
+    }
+
+    @PostMapping("/save")
+    public String save(RequestQuestion form) {
+
+        log.info("step:{} / {}", 3, form.toString());
+
+        return "question/step3";
+    }
+
+    @ResponseBody
+    @GetMapping("/test1")
+    public void test1(@SessionAttribute("requestQuestion") RequestQuestion form) {
+        log.info("test: {} / {}", 1, form.toString());
+    }
+
+    @ResponseBody
+    @GetMapping("/test2")
+    public void test2(HttpSession session) {
+        session.setAttribute("testField", "테스트 값...");
+    }
+
+    @ResponseBody
+    @GetMapping("/test3")
+    public void test3(@SessionAttribute("testField") String value) {
+        log.info("testField:" + value);
     }
 }
