66a73721d8898f7d5867fe57f66b776664c28ae1
yonggyo1125 <yonggyo00@kakao.com>
2024-11-18 14:48:45 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/question/QuestionController.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/question/QuestionController.java
@@ -1,27 +1,31 @@
 package org.koreait.question;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/question")
 public class QuestionController {
     @GetMapping("/step1")
-    public String step1() {
+    public String step1(@ModelAttribute RequestQuestion form) {
 
         return "question/step1";
     }
 
     @PostMapping("/step2")
-    public String step2() {
-
+    public String step2(RequestQuestion form) {
+        log.info("step:{} / {}", 1, form.toString());
         return "question/step2";
     }
 
     @PostMapping("/step3")
-    public String step3() {
+    public String step3(RequestQuestion form) {
+        log.info("step:{} / {}", 2, form.toString());
 
         return "question/step3";
     }
