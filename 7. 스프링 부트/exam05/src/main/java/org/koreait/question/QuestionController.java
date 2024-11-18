808de7aa3c7c237ee1e5b3989d903c44b847d7d7
yonggyo1125 <yonggyo00@kakao.com>
2024-11-18 15:04:18 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/question/QuestionController.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/question/QuestionController.java
@@ -2,17 +2,21 @@ package org.koreait.question;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 @RequestMapping("/question")
+@SessionAttributes("requestQuestion")   // requestQuestion 이름인 값은 session 범위에서 관리
 public class QuestionController {
+
+    @ModelAttribute("requestQuestion")
+    public RequestQuestion requestQuestion() {
+        return new RequestQuestion();
+    }
+
     @GetMapping("/step1")
-    public String step1(@ModelAttribute RequestQuestion form) {
+    public String step1() {
 
         return "question/step1";
     }
