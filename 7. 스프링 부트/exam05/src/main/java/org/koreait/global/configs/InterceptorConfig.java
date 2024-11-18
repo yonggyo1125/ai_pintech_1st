21b374548d2b1f825988ae1213a2b75947b024e8
yonggyo1125 <yonggyo00@kakao.com>
2024-11-18 15:54:10 +0900

    .

---
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/InterceptorConfig.java
@@ -0,0 +1,8 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+}
