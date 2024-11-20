16dcd3a309104df94702ff0c95fa6bbf65826dd2
yonggyo1125 <yonggyo00@kakao.com>
2024-11-20 17:04:31 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/global/advices/CommonControllerAdvice.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/advices/CommonControllerAdvice.java
@@ -3,6 +3,9 @@ package org.koreait.global.advices;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.exceptions.CommonException;
+import org.koreait.global.exceptions.scripts.AlertBackException;
+import org.koreait.global.exceptions.scripts.AlertException;
+import org.koreait.global.exceptions.scripts.AlertRedirectException;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
@@ -24,20 +27,42 @@ public class CommonControllerAdvice {
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 기본 응답 코드 500
         String message = e.getMessage(); // 기본 메세지
+        String tpl = "error/errorPage";
+        ModelAndView mv = new ModelAndView();
 
         if (e instanceof CommonException commonException) { // 직접 정의한 예외인 경우 정의한 예외로 대체
             status = commonException.getStatus();
             if (commonException.isErrorCode()) {
                 message = messageSource.getMessage(message, null, request.getLocale());
             }
+            StringBuffer sb = new StringBuffer(1000);
+
+            if (e instanceof AlertException) {
+                sb.append(String.format("alert('%s');", message));
+
+                tpl = "common/_execute_script";
+            }
+
+            if (e instanceof AlertBackException alertBackException) {
+                String target = alertBackException.getTarget();
+                sb.append(String.format("%s.history.back()", target));
+            }
+
+            if (e instanceof AlertRedirectException alertRedirectException) {
+                String target = alertRedirectException.getTarget();
+                String url = alertRedirectException.getUrl();
+                sb.append(String.format("%s.location.replace('%s')", target, url));
+            }
+
+            mv.addObject("script", sb.toString());
         }
 
         //response.setStatus(status.value());
 
-        ModelAndView mv = new ModelAndView();
+
         mv.setStatus(status);
         mv.addObject("message", message);
-        mv.setViewName("error/errorPage");
+        mv.setViewName(tpl);
 
         return mv;
     }
--- 7. 스프링 부트/exam05/src/main/java/org/koreait/member/exceptions/MemberNotFoundException.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/exceptions/MemberNotFoundException.java
@@ -1,9 +1,9 @@
 package org.koreait.member.exceptions;
 
-import org.koreait.global.exceptions.CommonException;
+import org.koreait.global.exceptions.scripts.AlertBackException;
 import org.springframework.http.HttpStatus;
 
-public class MemberNotFoundException extends CommonException {
+public class MemberNotFoundException extends AlertBackException {
     public MemberNotFoundException() {
         super("NotFound.member", HttpStatus.NOT_FOUND);
         setErrorCode(true);
