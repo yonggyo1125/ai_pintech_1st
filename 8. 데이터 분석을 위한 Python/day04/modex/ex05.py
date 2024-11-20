762697ef1cd2709d262a4c25ee4611cdb0772d9b
yonggyo1125 <yonggyo00@kakao.com>
2024-11-20 13:01:23 +0900

    .

---
+++ 8. 데이터 분석을 위한 Python/day04/modex/ex05.py
@@ -0,0 +1,6 @@
+import sys
+# print(sys.path)  # 파이썬 환경 변수, 파이썬이 공유할 수 있는 경로
+sys.path.append("D:/modex")
+
+import mod1
+print(mod1.add(10, 20))
--- 8. 데이터 분석을 위한 Python/day04/modex/mod2.py
+++
@@ -1,5 +0,0 @@
-PI = 3.141592
-
-class Math:
-    def solv(self, r):
-        return r ** 2 * PI
