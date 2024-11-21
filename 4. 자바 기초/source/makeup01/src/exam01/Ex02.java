abd10fae54f30ff96d39e28125c91e889e2158c5
yonggyo1125 <yonggyo00@kakao.com>
2024-11-21 19:52:15 +0900

    .

--- 4. 자바 기초/source/makeup01/src/exam01/Ex02.java
+++ 4. 자바 기초/source/makeup01/src/exam01/Ex02.java
@@ -1,4 +1,11 @@
 package exam01;
 
+import java.util.function.Function;
+
 public class Ex02 {
+    public static void main(String[] args) {
+        Function<Integer, String> func = x -> "*".repeat(x);
+        String stars = func.apply(3);
+        System.out.println(stars);
+    }
 }
