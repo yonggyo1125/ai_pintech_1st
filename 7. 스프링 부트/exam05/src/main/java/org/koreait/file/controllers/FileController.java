665ced3d3e3d02b16c3cdd329dcb1fda121bd897
yonggyo1125 <yonggyo00@kakao.com>
2024-11-22 14:33:57 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/file/controllers/FileController.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/file/controllers/FileController.java
@@ -1,9 +1,23 @@
 package org.koreait.file.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/file")
 public class FileController {
+
+    @GetMapping("/upload")
+    public String upload() {
+        return "file/upload";
+    }
+
+    @ResponseBody
+    @PostMapping("/upload")
+    public void uploadPs() {
+
+    }
 }
