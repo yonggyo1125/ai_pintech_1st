0f2996231773b46f53d6be62d37836142616b7e0
yonggyo1125 <yonggyo00@kakao.com>
2024-11-22 15:46:00 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/file/controllers/FileController.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/file/controllers/FileController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 @Controller
 @RequestMapping("/file")
@@ -17,7 +20,27 @@ public class FileController {
 
     @ResponseBody
     @PostMapping("/upload")
-    public void uploadPs(@RequestPart("file") MultipartFile _file) {
-        log.info(_file.toString());
+    public void uploadPs(@RequestPart("file") MultipartFile _file, @RequestPart("file2") MultipartFile file2) {
+        //log.info(_file.toString());
+        log.info("getName(): {}", _file.getName());  // <input .. name='이름'
+        log.info("getOriginalFilename(): {}", _file.getOriginalFilename()); // 업로드한 파일 이름
+        log.info("getSize(): {}", _file.getSize()); // 파일 용량(바이트)
+
+        // 업로드할 경로!
+        File uploadPath = new File("D:/uploads/" + _file.getOriginalFilename());
+        try {
+            _file.transferTo(uploadPath); // 임시 저장공간에 있는 파일 -> 지정한 서버 경로 이동
+        } catch (IOException e) {}
+
+        log.info("file2 : {}", file2.getOriginalFilename());
+    }
+
+    @PostMapping("/upload2")
+    public void uploadPs2(@RequestPart("file") MultipartFile[] files) {
+        for (MultipartFile file : files) {
+            try {
+                file.transferTo(new File("D:/uploads/" + file.getOriginalFilename()));
+            } catch (IOException e) {}
+        }
     }
 }
