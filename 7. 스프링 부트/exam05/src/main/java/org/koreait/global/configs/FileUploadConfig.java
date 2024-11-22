f7f9dc7733b83b58e90fc6ef5e1e6dae09105646
yonggyo1125 <yonggyo00@kakao.com>
2024-11-22 16:07:31 +0900

    .

---
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/FileUploadConfig.java
@@ -0,0 +1,22 @@
+package org.koreait.global.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FileUploadConfig implements WebMvcConfigurer {
+    @Value("${file.upload.path}")
+    private String filePath;
+
+    @Value("${file.upload.url}")
+    private String fileUrl;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(fileUrl + "**")
+                .addResourceLocations("file:///" + filePath);
+    }
+}
+
