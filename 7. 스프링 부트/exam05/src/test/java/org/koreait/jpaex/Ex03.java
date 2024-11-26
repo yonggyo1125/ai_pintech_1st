03abeea4a203b7f23dbfe9f3a61a938fa0332513
yonggyo1125 <yonggyo00@kakao.com>
2024-11-26 17:10:06 +0900

    .

--- 7. 스프링 부트/exam05/src/test/java/org/koreait/jpaex/Ex03.java
+++ 7. 스프링 부트/exam05/src/test/java/org/koreait/jpaex/Ex03.java
@@ -3,19 +3,15 @@ package org.koreait.jpaex;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.koreait.member.entities.Member;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
-@Transactional
-@SpringBootTest
 @ActiveProfiles({"default", "test"})
-public class Ex02 {
-
+@SpringBootTest
+public class Ex03 {
     @PersistenceContext
     private EntityManager em;
 
@@ -31,28 +27,6 @@ public class Ex02 {
         member.setRegDt(LocalDateTime.now());
 
         em.persist(member); // 영속 상태 - 변화 감지 상태
-        em.flush();
-    }
-
-    @Test
-    void test1() {
-        member.setName("(수정)사용자01");
-        em.flush();
-
-        em.detach(member); // 영속성 분리 상태 - 변화감지 X
-
-        member.setName("(수정2)사용자01");
-
-        em.flush();
-
-        /**
-         * 분리된 상태의 엔티티의 변경 상태를 비교하기 위해서
-         * SELECT 문이 한번 수행되고 DB와 차이가 있는 경우 UPDATE를 실행
-         */
-        em.merge(member); // 영속성 상태로 변경 - 변화감지 O
-
-        member.setPassword("(수정)12345678");
-
         em.flush();
     }
 }
