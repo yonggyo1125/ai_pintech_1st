a5b21e75ff7894be5e4d494415e5c774c0357887
yonggyo1125 <yonggyo00@kakao.com>
2024-11-26 16:53:05 +0900

    .

---
+++ 7. 스프링 부트/exam05/src/test/java/org/koreait/jpaex/Ex02.java
@@ -0,0 +1,9 @@
+package org.koreait.jpaex;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
+public class Ex02 {
+}
