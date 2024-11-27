3627bd84dbf109c8ce5ef2e0d1828088ded6a4e9
yonggyo1125 <yonggyo00@kakao.com>
2024-11-27 16:24:22 +0900

    .

--- 7. 스프링 부트/exam05/src/test/java/org/koreait/jpaex/Ex05.java
+++ 7. 스프링 부트/exam05/src/test/java/org/koreait/jpaex/Ex05.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -20,16 +21,19 @@ public class Ex05 {
 
     @BeforeEach
     void init() {
+        List<Member> members = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             Member member = new Member();
             member.setEmail("user" + i + "@test.org");
             member.setPassword("1234");
             member.setName("사용자" + i);
             member.setAuthority(Authority.USER);
-            repository.save(member);  // persist(...)
+            members.add(member);
+            //repository.save(member);  // persist(...)
         }
-
-        repository.flush(); //DB에 영구 반영
+        //repository.saveAll(members);
+       // repository.flush(); //DB에 영구 반영
+        repository.saveAllAndFlush(members);
     }
 
     @Test
