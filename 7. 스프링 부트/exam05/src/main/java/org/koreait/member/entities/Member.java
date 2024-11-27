5aa0a8ce6f23434b2df777138e565cf365286191
yonggyo1125 <yonggyo00@kakao.com>
2024-11-27 14:15:27 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/member/entities/Member.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/entities/Member.java
@@ -1,19 +1,31 @@
 package org.koreait.member.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+//@Table(name="KT_MEMBER")
 public class Member {
     @Id //  기본키
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long seq;
+
+    @Column(length=65, nullable = false, unique = true)
     private String email;
+
+    @Column(length=65, nullable = false)
     private String password;
+
+    @Column(length=40, nullable = false)
     private String name;
+
+    @Column(updatable = false)
     private LocalDateTime regDt;
+
+    @Column(insertable = false)
     private LocalDateTime modDt;
 }
