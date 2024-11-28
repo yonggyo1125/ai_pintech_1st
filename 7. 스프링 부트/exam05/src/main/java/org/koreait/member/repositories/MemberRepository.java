c47958a69219b3c483844922b9ac060e24bf573f
yonggyo1125 <yonggyo00@kakao.com>
2024-11-28 10:57:05 +0900

    .

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/member/repositories/MemberRepository.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/repositories/MemberRepository.java
@@ -3,9 +3,12 @@ package org.koreait.member.repositories;
 import org.koreait.member.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
   //Member findByEmail(String email);
     Optional<Member> findByEmail(String email);
+
+    List<Member> findByNameContainingOrderByRegDtDesc(String keyword);
 }
---
+++ 7. 스프링 부트/exam05/src/test/java/org/koreait/tests/Ex04.java
@@ -0,0 +1,38 @@
+package org.koreait.tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.koreait.member.constants.Authority;
+import org.koreait.member.entities.Member;
+import org.koreait.member.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
+public class Ex04 {
+    @Autowired
+    private MemberRepository repository;
+
+    @BeforeEach
+    void init() {
+        Faker faker = new Faker(Locale.KOREA);
+        List<Member> members = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            Member member = new Member();
+            member.setEmail(faker.internet().emailAddress());
+            member.setPassword(faker.regexify("[a-zA-Z0-9]{8,16}"));
+            member.setAuthority(Authority.USER);
+            member.setName(faker.name().fullName());
+            members.add(member);
+        }
+
+        repository.saveAllAndFlush(members);
+    }
+}
