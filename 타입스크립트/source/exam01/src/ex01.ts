5ca9e59841943d70a8405c1893de1586d9339a92
yonggyo1125 <yonggyo00@kakao.com>
2025-01-20 15:01:45 +0900

    .

--- 타입스크립트/source/exam01/src/ex01.ts
+++ 타입스크립트/source/exam01/src/ex01.ts
@@ -12,7 +12,8 @@ _name = 100
 let fruits: string[] = ['사과', '오렌지', '멜론']
 //fruits = '사과'
 
-let data: [string, number] = ['이름', 40]
+//let data: [string, number] = ['이름', 40]  // 첫번째는 문자형, 두번째는 숫자형
+let data: (string | number)[] = [40, '이름', '테스트'] // 순서 상관없이 문자형 또는 숫자형이 오는 타입
 //data = ['이름', '사십']
 
 // 변수 - 객체 타입
