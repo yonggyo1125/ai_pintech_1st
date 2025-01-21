f67d198f6299738f3a199d61b9f31a9c466cbc20
yonggyo1125 <yonggyo00@kakao.com>
2025-01-21 11:21:18 +0900

    .

--- 타입스크립트/source/exam02/src/ex01.ts
+++ 타입스크립트/source/exam02/src/ex01.ts
@@ -8,5 +8,29 @@
 //person = { name: '이이름' }
 
 /* 옵셔널 체이닝 ? - 타입은 한정하지만 필수는 아닌 속성 */
-let person: { name: string; age?: number }
-person = { name: '이이름' }
+//let person: { name: string; age?: number }
+//person = { name: '이이름' }
+
+/* any 타입 */
+//let a; // any 타입
+//let a: any // 모든 타입 가능
+//a = 100
+//a = 'ABC'
+
+/* unknown 타입 */
+/*
+let a: unknown
+a = 'ABC'
+//a.toUpperCase() // 에러 발생, 타입 체크가 반드시 필요(unknown)
+if (typeof a === 'string') {
+  a.toUpperCase()
+} */
+
+/* 함수 타입 */
+/*
+function printName(name: string, formatter: (name: string) => string): void {
+  console.log(formatter(name))
+}
+
+printName('이이름', (name) => `${name}님`)
+*/
