8cf38c228568bfde2e664a8e7c68f94260797e01
yonggyo1125 <yonggyo00@kakao.com>
2025-01-22 13:20:49 +0900

    .

--- 리액트/exam01/src/components/Counter.tsx
+++ 리액트/exam01/src/components/Counter.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 
 const Counter = () => {
   const [number, setNumber] = useState<number>(1)
-
+  console.log('렌더링!')
   return (
-    <div>
+    <>
+      <br />
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>+1</button>
       <button onClick={() => setNumber(number - 1)}>-1</button>
-    </div>
+    </>
   )
 }
 
