ba0998637e61a99e1073e8ab1055ad442fe1223e
yonggyo1125 <yonggyo00@kakao.com>
2025-01-23 10:52:57 +0900

    .

--- 리액트/exam01/src/components/Greeting.tsx
+++ 리액트/exam01/src/components/Greeting.tsx
@@ -1,10 +1,25 @@
+//import '../styles/greeting'
+import '@styles/greeting'
 const Greeting = () => {
   const name = '이이름'
   const date = new Date()
+  const visible = true
+
+  /*
+  const style = {
+    backgroundColor: 'skyblue',
+    color: 'blue',
+  }
+  */
   return (
     <>
-      <h1>{name}님 반값습니다.</h1>
-      <h2>현재 시간: {date.toLocaleString()}</h2>
+      <h1 className="title">{name}님 반갑습니다.</h1>
+      {/*
+      <h1 style={{ backgroundColor: 'skyblue', color: 'blue' }}>
+        {name}님 반값습니다.
+      </h1> */}
+      {/* visible ? <h2>현재 시간: {date.toLocaleString()}</h2> : '' */}
+      {visible && <h2>현재 시간: {date.toLocaleString()}</h2>}
     </>
   )
 }
