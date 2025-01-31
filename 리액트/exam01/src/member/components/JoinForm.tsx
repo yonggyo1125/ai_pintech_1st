1567114d44d83040c1378515824fa8c96593a82a
yonggyo1125 <yonggyo00@kakao.com>
2025-01-31 12:55:49 +0900

    .

--- 리액트/exam01/src/member/components/JoinForm.tsx
+++ 리액트/exam01/src/member/components/JoinForm.tsx
@@ -1,29 +1,29 @@
 'use client'
-const JoinForm = ({ onSubmit }): React.ReactNode => {
+const JoinForm = ({ onSubmit, onChange }): React.ReactNode => {
   return (
     <form onSubmit={onSubmit}>
       <dl>
         <dt>이메일</dt>
         <dd>
-          <input type="text" />
+          <input type="text" name="email" onChange={onChange} />
         </dd>
       </dl>
       <dl>
         <dt>비밀번호</dt>
         <dd>
-          <input type="password" />
+          <input type="password" name="password" onChange={onChange} />
         </dd>
       </dl>
       <dl>
         <dt>비밀번호 확인</dt>
         <dd>
-          <input type="password" />
+          <input type="password" name="confirmPassword" onChange={onChange} />
         </dd>
       </dl>
       <dl>
         <dt>회원명</dt>
         <dd>
-          <input type="text" />
+          <input type="text" name="name" onChange={onChange} />
         </dd>
       </dl>
       <button type="submit">가입하기</button>
