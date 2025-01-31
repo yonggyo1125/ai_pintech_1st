5e78dd6aa320edb798d0e3e324d66b29dd51552e
yonggyo1125 <yonggyo00@kakao.com>
2025-01-31 10:50:25 +0900

    .

--- 리액트/exam01/src/components/ColorBox.tsx
+++ 리액트/exam01/src/components/ColorBox.tsx
@@ -1,5 +1,9 @@
+'use client'
 const ColorBox = (): React.ReactNode => {
-  return <></>
+  //const handleClick = () => alert('테스트')
+
+  //return <button onClick={handleClick}>클릭</button>\
+  return <button onClick={() => alert('테스트')}>클릭</button>
 }
 
 export default ColorBox
