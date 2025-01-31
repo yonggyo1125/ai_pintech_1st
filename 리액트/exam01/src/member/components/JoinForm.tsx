9e703ced51540953a82bdda887e56c99a2cc9780
yonggyo1125 <yonggyo00@kakao.com>
2025-01-31 13:21:19 +0900

    .

--- 리액트/exam01/src/member/components/JoinForm.tsx
+++ 리액트/exam01/src/member/components/JoinForm.tsx
@@ -1,31 +1,57 @@
 'use client'
-const JoinForm = ({ onSubmit, onChange }): React.ReactNode => {
+import { FaRegSquareCheck, FaSquareCheck } from 'react-icons/fa6'
+
+const JoinForm = ({ onSubmit, onChange, onToggle, form }): React.ReactNode => {
   return (
     <form onSubmit={onSubmit}>
       <dl>
         <dt>이메일</dt>
         <dd>
-          <input type="text" name="email" onChange={onChange} />
+          <input
+            type="text"
+            name="email"
+            value={form.email}
+            onChange={onChange}
+          />
         </dd>
       </dl>
       <dl>
         <dt>비밀번호</dt>
         <dd>
-          <input type="password" name="password" onChange={onChange} />
+          <input
+            type="password"
+            name="password"
+            value={form.password}
+            onChange={onChange}
+          />
         </dd>
       </dl>
       <dl>
         <dt>비밀번호 확인</dt>
         <dd>
-          <input type="password" name="confirmPassword" onChange={onChange} />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={form.confirmPassword}
+            onChange={onChange}
+          />
         </dd>
       </dl>
       <dl>
         <dt>회원명</dt>
         <dd>
-          <input type="text" name="name" onChange={onChange} />
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={onChange}
+          />
         </dd>
       </dl>
+      <div onClick={onToggle}>
+        {form.agree ? <FaSquareCheck /> : <FaRegSquareCheck />} 회원가입 약관에
+        동의합니다.
+      </div>
       <button type="submit">가입하기</button>
     </form>
   )
--- 리액트/exam01/src/member/containers/JoinContainer.tsx
+++ 리액트/exam01/src/member/containers/JoinContainer.tsx
@@ -3,16 +3,23 @@ import { useState } from 'react'
 import JoinForm from '../components/JoinForm'
 
 type FormType = {
-  email: string
-  password: string
-  confirmPassword: string
-  name: string
+  email?: string
+  password?: string
+  confirmPassword?: string
+  name?: string
+  agree?: boolean
 }
 
 const JoinContainer = (): React.ReactNode => {
-  const [form, setForm] = useState<FormType>()
+  const [form, setForm] = useState<FormType>({ email: 'user01@test.org' })
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleToggle = () => {
+    setForm({ ...form, agree: !Boolean(form.agree) })
+  }
 
   /**
    * 양식 제출시 처리 함수
@@ -20,9 +27,24 @@ const JoinContainer = (): React.ReactNode => {
   const onSubmit = (e) => {
     // form 태그의 기본 동작 차단
     e.preventDefault()
+
+    /**
+     * 양식 검증
+     * 1) 필수 항목 검증
+     * 2)
+     */
   }
 
-  return <JoinForm onSubmit={onSubmit} onChange={handleChange} />
+  return (
+    <>
+      <JoinForm
+        onSubmit={onSubmit}
+        onChange={handleChange}
+        onToggle={handleToggle}
+        form={form}
+      />
+    </>
+  )
 }
 
 export default JoinContainer
