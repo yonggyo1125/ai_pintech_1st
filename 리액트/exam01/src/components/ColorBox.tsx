b8a4226cf7f703ffdf5891f8363ff0660459c203
yonggyo1125 <yonggyo00@kakao.com>
2025-01-31 10:54:57 +0900

    .

--- 리액트/exam01/src/components/ColorBox.tsx
+++ 리액트/exam01/src/components/ColorBox.tsx
@@ -1,9 +1,16 @@
 'use client'
-const ColorBox = (): React.ReactNode => {
-  //const handleClick = () => alert('테스트')
+import { useState, useLayoutEffect } from 'react'
 
-  //return <button onClick={handleClick}>클릭</button>\
-  return <button onClick={() => alert('테스트')}>클릭</button>
+const ColorBox = (): React.ReactNode => {
+  const [colors, setColors] = useState<string[]>([])
+  const [selected, setSelected] = useState<string>('gray')
 }
 
+/*
+const ColorBox = (): React.ReactNode => {
+  //const handleClick = () => alert('테스트')
+  //return <button onClick={handleClick}>클릭</button>\
+  //return <button onClick={() => alert('테스트')}>클릭</button>
+}
+*/
 export default ColorBox
