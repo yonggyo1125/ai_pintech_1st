374d0eacc1d72b126af5e244f8b38280f95eafd5
yonggyo1125 <yonggyo00@kakao.com>
2025-02-03 13:18:37 +0900

    .

--- 리액트/exam02/src/app/member/components/LoginForm.tsx
+++ 리액트/exam02/src/app/member/components/LoginForm.tsx
@@ -1,17 +1,27 @@
-const LoginForm = () => {
+const LoginForm = ({ form, onChange }) => {
   return (
     <>
       <form>
         <dl>
           <dt>이메일</dt>
           <dd>
-            <input type="text" name="email" />
+            <input
+              type="text"
+              name="email"
+              value={form?.email ?? ''}
+              onChange={onChange}
+            />
           </dd>
         </dl>
         <dl>
           <dt>비밀번호</dt>
           <dd>
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={form?.password ?? ''}
+              onChange={onChange}
+            />
           </dd>
         </dl>
         <button type="submit">로그인</button>
--- 리액트/exam02/src/app/member/containers/LoginContainer.tsx
+++ 리액트/exam02/src/app/member/containers/LoginContainer.tsx
@@ -1,6 +1,13 @@
+'use client'
+import { useState } from 'react'
 import LoginForm from '../components/LoginForm'
 const LoginContainer = () => {
-  return <LoginForm />
+  const [form, setForm] = useState<{ email?: string; password?: string }>({})
+
+  const handleChange = (e) =>
+    setForm((form) => ({ ...form, [e.target.name]: e.target.value }))
+
+  return <LoginForm form={form} onChange={handleChange} />
 }
 
 export default LoginContainer
