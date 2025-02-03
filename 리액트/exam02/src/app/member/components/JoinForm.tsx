9cd49ae61d91baca96f4f02a34b3b9ef23aa2c52
yonggyo1125 <yonggyo00@kakao.com>
2025-02-03 11:48:43 +0900

    .

--- 리액트/exam02/src/app/member/components/JoinForm.tsx
+++ 리액트/exam02/src/app/member/components/JoinForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 const JoinForm = ({ actionState }) => {
-  const [state, formAction, isPending] = actionState
+  const [errors, formAction, isPending] = actionState
   return (
     <>
       <form action={formAction}>
@@ -8,24 +8,30 @@ const JoinForm = ({ actionState }) => {
           <dt>이메일</dt>
           <dd>
             <input type="text" name="email" />
+            {errors?.email && errors.email.map((m) => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <dl>
           <dt>비밀번호</dt>
           <dd>
             <input type="password" name="password" />
+            {errors?.password &&
+              errors.password.map((m) => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <dl>
           <dt>비밀번호 확인</dt>
           <dd>
             <input type="password" name="confirmPassword" />
+            {errors?.confirmPassword &&
+              errors.confirmPassword.map((m) => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <dl>
           <dt>회원명</dt>
           <dd>
             <input type="text" name="name" />
+            {errors?.name && errors.name.map((m) => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <button type="submit" disabled={isPending}>
--- 리액트/exam02/src/app/member/containers/JoinContainer.tsx
+++ 리액트/exam02/src/app/member/containers/JoinContainer.tsx
@@ -1,12 +1,19 @@
 'use client'
 import JoinForm from '../components/JoinForm'
 import { processJoin } from '../services/actions'
-import { useActionState } from 'react'
+import { useActionState, useState } from 'react'
+
+type FormType = {
+  email?: string
+  password?: string
+  confirmPassword?: string
+  name?: string
+  agree?: boolean
+}
 
 const JoinContainer = () => {
-  const actionState = useActionState(processJoin, {
-    email: '',
-  })
+  const [form, setForm] = useState<FormType>({})
+  const actionState = useActionState(processJoin, {})
   return <JoinForm actionState={actionState} />
 }
 
