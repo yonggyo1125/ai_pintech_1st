ef1d95c26af54f654af9d43afd5bdf7be8bfa918
yonggyo1125 <yonggyo00@kakao.com>
2025-02-05 10:37:04 +0900

    .

--- 리액트/exam02/src/app/exam03/page.tsx
+++ 리액트/exam02/src/app/exam03/page.tsx
@@ -4,6 +4,17 @@ import { useState } from 'react'
 const Exam03Page = () => {
   const [numbers, setNumbers] = useState<number[]>([])
   const [number, setNumber] = useState<number | undefined>()
+  
+  const handleChange = (e) => setNumber(e.target.value)
+  const handleClick = () => {
+    if (number && !isNaN(number)) {
+      setNumbers(numbers.concat(number))
+    }
+  }
+
+  return (
+
+  )
 }
 
 export default Exam03Page
