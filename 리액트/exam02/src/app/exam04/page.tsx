dc3cc7b24ebf29f8c33a0623f76a0c03a0dfe337
yonggyo1125 <yonggyo00@kakao.com>
2025-02-05 11:50:50 +0900

    .

---
+++ 리액트/exam02/src/app/exam04/page.tsx
@@ -0,0 +1,13 @@
+import './box.sass'
+const Exam04Page = () => {
+  return (
+    <div className="box-group">
+      <div className="box red"></div>
+      <div className="box blue"></div>
+      <div className="box orange"></div>
+      <div className="box gray"></div>
+    </div>
+  )
+}
+
+export default Exam04Page
