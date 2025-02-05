e7da87f2ae55aa59db84c1fbd21a65e8ab0591b4
yonggyo1125 <yonggyo00@kakao.com>
2025-02-05 10:21:29 +0900

    .

--- 리액트/exam02/src/app/exam01/page.tsx
+++ 리액트/exam02/src/app/exam01/page.tsx
@@ -1,9 +1,18 @@
 'use client'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 const Counter = () => {
   const [number, setNumber] = useState<number>(0)
   let num = useRef(1)
+
+  useEffect(() => {
+    console.log('useEffect()')
+    console.log('number', number)
+
+    return () => {
+      console.log('뒷정리 함수 - 언마운트시에 호출')
+    }
+  }, [number])
 
   const onIncrease = () => {
     setNumber(number + 1)
---
+++ 리액트/exam02/src/app/exam03/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { useState } from 'react'
+
+const Exam03Page = () => {
+  const [numbers, setNumbers] = useState<number[]>([])
+  const [number, setNumber] = useState<number | undefined>()
+}
+
+export default Exam03Page
